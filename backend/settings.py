6c2a06ee8f2a81c1b49c1507bb744d5f55670d54
Aleksander Pucher <aleksander.pucher@nokia.com>
2020-10-31 12:06:48 +0100

    add isort

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 SETTINGS_DIR = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(SETTINGS_DIR)
